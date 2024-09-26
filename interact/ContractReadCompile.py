81b0d30cb0b912dd4c1bb82c37f2fbc28a77e931
itay601 <itay601@LAPTOP-UI69H6FF>
2024-09-26 10:31:34 +0300

    Add simple interaction functions and upgrading Contract

---
+++ interact/ContractReadCompile.py
@@ -0,0 +1,41 @@
+from solcx import compile_standard, install_solc  
+import json
+import os
+from dotenv import load_dotenv
+
+
+def compile_Contarct():
+    install_solc('0.8.27')
+    
+    contract_code = read_contract()
+    compile_solidity = compile_standard({
+        "language": "Solidity",
+        "sources": {"PokerContract.sol": {
+            "content": contract_code
+        }},
+        "settings": {
+            "outputSelection": {
+                "*": {"*":
+                    ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        }
+    },solc_version = "0.8.27")
+    return compile_solidity
+
+
+
+def convert_contract_to_json():
+    with open("compiled_code.json", "w") as file:
+        json.dump(compile_Contarct(), file)
+
+
+
+def read_contract():
+    load_dotenv()
+    contract_path = os.getenv("CONTRACT_PATH")
+    try:
+        with open(contract_path, 'r') as file:
+            contract_contents = file.read()
+            return contract_contents
+    except Exception as e:
+        print(f'Error reading file: {e}')   
